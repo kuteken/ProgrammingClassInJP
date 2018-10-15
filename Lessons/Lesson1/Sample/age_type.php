e66dbd528b5312137280fdf08c3ceb17f3f797ea
Kuteken <kuteken@gmail.com>
2018-10-15 13:31:37 +0700

    Update age_type.php

--- Lessons/Lesson1/Sample/age_type.php
+++ Lessons/Lesson1/Sample/age_type.php
@@ -1,11 +1,22 @@
 <?php
 
-$age = 12;
+// 実行するときは、ターミナルで php age_type.php {age}
+// e.g. php age_type.php 12
 
-if ($age > 20) {
+$age = $argv[1];
+
+if ($age < 20) {
     $type = '子供';
-} else {
+} elseif ($age < 60) {
     $type = '大人';
+} elseif ($age < 150) {
+    $type = 'お年寄り';
+} else {
+    $type = 'エラー';
 }
 
-print "あなたは$typeです。\n";
+if ($type != 'エラー') {
+    print "あなたは{$type}です。\n";
+} else {
+    print "エラーです。正しい年齢を入力してください。\n";
+}
