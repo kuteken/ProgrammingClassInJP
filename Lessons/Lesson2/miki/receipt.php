ac8c353e0d7c317bf1ecfdaad3ebbec81cb017e4
kuteken <kuteken@gmail.com>
2018-10-18 22:36:34 +0700

    Add sample basic program for Receipt output

--- Lessons/Lesson2/miki/receipt.php
+++ Lessons/Lesson2/miki/receipt.php
@@ -2,41 +2,47 @@
 define("TAX_RATE", 10); // Percentage
 
 $product_list = [
-    'Coca' => 10000,
-    'Pepsi' => 10000,
-    'Sprite' => 15000,
+    'Coca'    => 10000,
+    'Pepsi'   => 10000,
+    'Sprite'  => 15000,
     'Redbull' => 20000,
 ];
 
-// $product1 = $argv[2]; // 第2引数
-// $product2 = $argv[3]; // 第3引数
-// print $product1;
-// print $product2;
-
-$product_number = $argc - 2;
-for ($i = 2; $i <= $product_number + 1; $i++) {
-    $product_name = $argv[$i];
-    $shopping_cart[$product_name] = "";
+// 商品リスト
+$filename = array_shift($argv);
+$shopping_cart = $argv;
+foreach ($shopping_cart as $product) {
+    $bought_products[$product] = NULL;
 }
-var_dump($product_list, $shopping_cart, array_ingit nitersect_key($product_list, $shopping_cart));
+$bought_products = array_intersect_key($product_list, $bought_products);
 
-$subtotal = $argv[1];
+// 小計計算
+$subtotal = 0;
+$product_receipt = "";
+foreach ($bought_products as $name => $price) {
+    $subtotal += $price;
+    $product_receipt .= "{$name}\t\t{$price}\n";
+}
+
+// 消費税計算
 $tax_rate = TAX_RATE;
-$tax      = $subtotal * $tax_rate / 100;
-$total    = $subtotal + $tax;
+$tax = $subtotal * $tax_rate / 100;
 
+// 合計計算
+$total = $subtotal + $tax;
+
+// レシート作成
 $result = <<<"EOT"
 ===========================
 Receipt
 ---------------------------
-Coca              10.000
-Redbull           20.000
+{$product_receipt}---------------------------
+Subtotal\t{$subtotal}
+Tax(10%)\t{$tax}
 ---------------------------
-Subtotal          {$subtotal}
-Tax(10%)          {$tax}
----------------------------
-Total             {$total}
+Total\t\t{$total}
 ===========================
+
 EOT;
 
 print $result;
