ec16a3e5e2adfb1a2e0d5ef8a2841ff84c7c0528
Duong-Tu-Mulodo <44149376+Duong-Tu-Mulodo@users.noreply.github.com>
2018-10-24 19:07:05 +0700

    Create foreach.php

---
+++ Tu/Lesson4/foreach.php
@@ -0,0 +1,16 @@
+<?php
+//$array = array("A","B","C");
+//foreach ($array as $value){
+//    echo $value;
+//}
+
+$array = [
+    'Miki'=> 30,
+    'Na' => 20,
+    'Huy' => 10
+];
+
+foreach ($array as $name => $age){
+    echo "{$name} is {$age} years old! \n";
+}
+
